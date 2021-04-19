48d4e8cf9ac57b8fbd18697ac73fbbf8b9b8ca1f
lynchxd <michelantoine.charnay@gmail.com>
2021-04-19 13:22:17 -0400

    comit final

--- frutas.js
+++
@@ -1,9 +0,0 @@
-const frutas = ['platano', 'manzana', 'platano', 'pera'];
-const dinero = 1000;
-
-//exportación de módulo. Se exportan los 2 dentro de un objeto.
-
-module.exports = {
-    frutas : frutas,
-    dinero : dinero
-}
